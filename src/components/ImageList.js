6fc16cd67f7acef4ce76f540ac9ffa953bbec2bf
Duygu Temur <duygutemur@archis.company>
2020-11-21 23:29:52 +0300

    refactor data name

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 
 const ImageList = (props) => {
-    const images = props.images.map((image) => {
-        return <img key={image.id} src={image.urls.regular} alt="images" />
+    const images = props.images.map(({description, id, urls}) => {
+        return <img key={id} src={urls.regular} alt={description} />
     })
 
     return(
